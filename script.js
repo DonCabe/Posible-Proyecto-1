ed502d9dd40ede1bfe0f14f552cf0c96fdcf2e8b
unknown <eperespicaro@gmail.com>
2024-01-15 20:16:17 -0300

    Primeros pasos del posible proyecto

---
+++ script.js
@@ -0,0 +1,6 @@
+let teGusta = confirm('Te gustan los juegos de mesa?');
+let juegoFavorito = prompt('Cual es tu juego de mesa favorito?');
+let juegosDisponibles = 40;
+console.log('El usuario respondió `' + teGusta + '` a si le gustan o no los juegos de mesa.');
+console.log('El juego favorito del usuario es ' + juegoFavorito + ' y hay ' + juegosDisponibles + ' juegos disponibles!');
+alert('En breve buscaremos ' + juegoFavorito + '. Esperamos que disfrutes tu juego!');
